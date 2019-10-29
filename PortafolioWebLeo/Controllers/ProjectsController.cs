2b7101bd9f27c71d214ca0e77f8a0d431e9ec3f9
Leonardo Fabián Hernández Peña <leonardohernandez.fp@academia.umb.edu.co>
2019-10-28 21:35:31 -0500

    se crea un CRUD para el proyecto

--- PortafolioWebLeo/Controllers/ProjectsController.cs
+++ PortafolioWebLeo/Controllers/ProjectsController.cs
@@ -3,14 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PortafolioWebLeo.Models;
+using PortafolioWebLeo.Services;
 
 namespace PortafolioWebLeo.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class ProjectsController : Controller
     {
-        public IActionResult Index()
+        private readonly ProjectService _projectService;
+
+        public ProjectsController(ProjectService projectService)
         {
-            return View();
+            _projectService = projectService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Project>> Get() =>
+            _projectService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetProject")]
+        public ActionResult<Project> Get(string id)
+        {
+            var project = _projectService.Get(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return project;
+        }
+
+        [HttpPost]
+        public ActionResult<Project> Create(Project project)
+        {
+            _projectService.Create(project);
+
+            return CreatedAtRoute("GetProject", new { id = project.Id.ToString() }, project);
+        }
+        [HttpPut]
+        public IActionResult Update(string id, Project projectIn)
+        {
+            var project = _projectService.Get(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+            _projectService.Update(id, projectIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var project = _projectService.Get(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+            _projectService.Remove(project.Id);
+
+            return NoContent();
         }
     }
 }
